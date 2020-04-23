23c44e9eeffb36d5d33d30bb938f898be1ec571a
Averkij <averoo@gmail.com>
2020-04-23 13:03:03 +0300

    Fix.

--- docker/script.py
+++ docker/script.py
@@ -11,6 +11,12 @@ tokensPath = '/tmp/data/input_tokens.txt'
 import jieba
 
 with open(inputPath, mode='r', encoding='utf-8') as input, open(tokensPath, mode='w', encoding='utf-8') as tokens:
-	for line in input:
-		t = jieba.lcut(line)
-		tokens.write(' '.join(t))
+	lines = input.readlines()
+
+	#do not print \n for the last line
+	for line in lines[:-1]:
+		tokenized = ' '.join([x.strip() for x in jieba.lcut(line) if len(x.strip())>0])
+		tokens.write(tokenized + '\n')
+
+	tokenized = ' '.join([x.strip() for x in jieba.lcut(lines[-1]) if len(x.strip())>0])
+	tokens.write(tokenized)
