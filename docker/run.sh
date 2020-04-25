f7e84d4aaa660846f484d9862739ad6abe006d1a
Averkij <averoo@gmail.com>
2020-04-25 22:25:48 +0300

    Translation with tokenization.

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python /script.py
 mv /china2-model_step_55000.pt /OpenNMT-py/
-cd /OpenNMT-py && python translate.py -model china2-model_step_55000.pt -src /tmp/data/input_tokens.txt -output /opt/results/pre_output.txt -replace_unk -verbose
-python /script2.py
+cd /OpenNMT-py && python translate.py -model china2-model_step_55000.pt -src /tmp/data/input_tokens.txt -output /opt/results/output.txt -replace_unk -verbose
+#python /script2.py
--- docker/script.py
+++ docker/script.py
@@ -5,18 +5,12 @@ inputPath = '/tmp/data/input.txt'
 tokensPath = '/tmp/data/input_tokens.txt'
 #outputPath = '/opt/data/output.txt'
 
-#inputPath = 'D:\\git\\input.txt'
-#tokensPath = 'D:\\git\\input_tokens.txt'
+#inputPath = 'D:\\git\\output_zh_test.txt'
+#tokensPath = 'D:\\git\\output_zh_test_tokens.txt'
 
 import jieba
 
 with open(inputPath, mode='r', encoding='utf-8') as input, open(tokensPath, mode='w', encoding='utf-8') as tokens:
-	lines = input.readlines()
-
-	#do not print \n for the last line
-	for line in lines[:-1]:
-		tokenized = ' '.join([x.strip() for x in jieba.lcut(line) if len(x.strip())>0])
+	for line in input:
+		tokenized = ' '.join([x.strip() for x in jieba.lcut(line)])
 		tokens.write(tokenized + '\n')
-
-	tokenized = ' '.join([x.strip() for x in jieba.lcut(lines[-1]) if len(x.strip())>0])
-	tokens.write(tokenized)
