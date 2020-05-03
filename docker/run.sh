23f3e9d14a2f637b642f75bb1bdf3ac2ff654e23
Averkij <averoo@gmail.com>
2020-05-03 08:23:42 +0300

    Wrangling with weights.

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-python /script.py
-mv /china2-model_step_55000.pt /OpenNMT-py/
-cd /OpenNMT-py && python translate.py -model china2-model_step_55000.pt -src /tmp/data/input_tokens.txt -output /opt/results/output.txt -replace_unk -verbose
-#python /script2.py
+#python /script.py
+#mv /ondatr-model_step_50000.pt /OpenNMT-py/
+python /OpenNMT-py/tools/apply_bpe.py -c /src.code -i /tmp/data/input.txt -o /input.bpe
+python /OpenNMT-py/translate.py -model /ondatr-trans_step_130000_v32_ted144.pt -src /input.bpe -output /output.bpe -replace_unk -verbose
+sed 's/@@ //g' /output.bpe > /opt/results/output.txt
+#mv /output.bpe /opt/results/output.txt
