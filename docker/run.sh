090b01f23624f84f31312fecca8317ce42bbdfb3
Averkij <averoo@gmail.com>
2020-05-07 13:13:31 +0300

    Update docker container.

--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #python /script.py
 #mv /ondatr-model_step_50000.pt /OpenNMT-py/
-python /apply_bpe.py -c /src_ted217_32.code -i /tmp/data/input.txt -o /input.bpe
-python /OpenNMT-py/translate.py -model /ondatr-trans_step_100000_ted217_v32_lower.pt -src /input.bpe -output /output.bpe -replace_unk -verbose
+python /apply_bpe.py -c /src_news_32.code -i /tmp/data/input.txt -o /input.bpe
+python /OpenNMT-py/translate.py -model /ondatr-trans_step_180000_news250_32.pt -src /input.bpe -output /output.bpe -replace_unk -verbose
 sed 's/@@ //g' /output.bpe > /opt/results/output.txt
 #mv /output.bpe /opt/results/output.txt
